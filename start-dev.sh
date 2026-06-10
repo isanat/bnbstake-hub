1c72519057168babf93c6dbd79c909b6bc98d48c
isanat <netlinkassist@gmail.com>
2026-06-10 21:11:18 +0000

    chore: Clean up temp files

--- start-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-export DATABASE_URL="postgresql://bnbstake:npg_jNY8Hka6iJGc@ep-dawn-cloud-ai27xf2d-pooler.c-4.us-east-1.aws.neon.tech/bnbstake?sslmode=require"
-export DIRECT_URL="postgresql://bnbstake:npg_jNY8Hka6iJGc@ep-dawn-cloud-ai27xf2d.c-4.us-east-1.aws.neon.tech/bnbstake?sslmode=require"
-while true; do
-  npx next dev -p 3000
-  echo "Server crashed, restarting in 3s..."
-  sleep 3
-done
